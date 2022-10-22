0910efac8f6cd84a4bb426e488ee673bad9dbfe3
Shantanu <sborkar9545@gmail.com>
2022-10-22 23:45:58 +0530

    Conditional Rendering for SignOut Button

--- lib/API/data_manager.dart
+++ lib/API/data_manager.dart
@@ -4,10 +4,14 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:leo_anime/API/trending_data_model.dart';
 
+// Fetching and Storing Data in local storage
+
 class DataManager {
   List<RecentRelease>? recentEpList;
   List<MostPopular>? mostPopularList;
   List<Trending>? trendingList;
+
+  // Fetch Recently Released Anime and append to list
   fetchRecent() async {
     const url = "https://api.consumet.org/meta/anilist/recent-episodes";
     var response = await http.get(Uri.parse(url));
@@ -23,6 +27,7 @@ class DataManager {
     }
   }
 
+  //  Fetch Most Popular Anime and append to list
   fetchPopular() async {
     const url = "https://api.consumet.org/meta/anilist/popular";
     var response = await http.get(Uri.parse(url));
@@ -36,38 +41,38 @@ class DataManager {
     }
   }
 
-  fetchTrending()async{
+  // Fetch Trending Anime and append to list
+  fetchTrending() async {
     const url = "https://api.consumet.org/meta/anilist/trending";
     var response = await http.get(Uri.parse(url));
-    if(response.statusCode==200){
+    if (response.statusCode == 200) {
       trendingList = [];
-      var data  = jsonDecode(response.body) as Map<String,dynamic>;
+      var data = jsonDecode(response.body) as Map<String, dynamic>;
       List<dynamic> decodeData = data['results'];
-      for(var json in decodeData){
+      for (var json in decodeData) {
         trendingList?.add(Trending.fromJson(json));
       }
     }
   }
 
+  // Fetch searched anime and return all matching ones
   Future<List<dynamic>> searchAnime(query) async {
-
     String url = "https://api.consumet.org/meta/anilist/$query";
     var response = await http.get(Uri.parse(url));
-    List<dynamic>?searchList =[];
-    if(response.statusCode == 200){
+    List<dynamic>? searchList = [];
+    if (response.statusCode == 200) {
       var data = jsonDecode(response.body) as Map<String, dynamic>;
       List<dynamic> decodeData = data['results'];
-      for(var json in decodeData){
+      for (var json in decodeData) {
         searchList.add(Trending.fromJson(json));
       }
     }
     return searchList;
-    
-
   }
 
+  // Getters
   Future<List<Trending>> getTrending() async {
-    if(trendingList==null)await fetchTrending();
+    if (trendingList == null) await fetchTrending();
     return trendingList!;
   }
 
