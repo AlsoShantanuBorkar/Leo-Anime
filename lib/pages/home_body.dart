4edf03a94e6ae99347241c50f2288483cc2d972d
Shantanu <sborkar9545@gmail.com>
2022-10-22 23:48:24 +0530

    removed whitespace

---
+++ lib/pages/home_body.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:leo_anime/API/data_manager.dart';
+import 'package:leo_anime/components/most_popular_list.dart';
+import 'package:leo_anime/components/recent_release_list.dart';
+import 'package:leo_anime/components/trending_list.dart';
+
+class HomeBody extends StatelessWidget {
+  final DataManager dataManager;
+  const HomeBody({super.key, required this.dataManager});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: ListView(
+        shrinkWrap: false,
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              "Recent Releases",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 25,
+                letterSpacing: 2
+              ),
+            ),
+          ),
+          RecentReleaseList(
+            dataManager: dataManager,
+          ),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              "Most Popular",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 25,
+                letterSpacing: 2
+              ),
+            ),
+          ),
+          MostPopularList(
+            dataManager: dataManager,
+          ),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              "Trending",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 25,
+                letterSpacing: 2
+              ),
+            ),
+          ),
+          TrendingList(
+            dataManager: dataManager,
+          ),
+          
+        ],
+      ),
+    );
+  }
+}
