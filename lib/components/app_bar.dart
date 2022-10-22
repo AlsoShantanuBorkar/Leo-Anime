477e12c630dad74639c96a025ec182341085fa1b
Shantanu <sborkar9545@gmail.com>
2022-10-22 23:46:31 +0530

    Conditional Rendering of SignOut Icon

--- lib/components/app_bar.dart
+++ lib/components/app_bar.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:leo_anime/pages/authentication/user_model.dart';
+import 'package:leo_anime/services/auth.dart';
+import 'package:provider/provider.dart';
 
+// Custom App Bar for HomePage,Search Page , Bookmarks and Authentication Pages
 class CustomAppBar extends StatelessWidget {
   final String title;
   const CustomAppBar({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
+    final AuthService auth = AuthService();
+    final userData = Provider.of<AppUser?>(context);
+
+    // If the user is not Signed In yet do not display the Sign In icon
+    dynamic actionsList = userData != null
+        ? [
+            TextButton.icon(
+                onPressed: () async {
+                  await auth.signOutUser();
+                },
+                icon: const Icon(
+                  Icons.exit_to_app,
+                  color: Color.fromRGBO(255, 255, 255, 1),
+                ),
+                label: const Text(""))
+          ]
+        : null;
     return AppBar(
+      actions: actionsList,
       iconTheme: const IconThemeData(color: Color.fromRGBO(27, 30, 47, 1)),
       elevation: 10,
       scrolledUnderElevation: 10,
