031c14db776aa97f22166a3a1338f2f7612daec8
Shantanu <sborkar9545@gmail.com>
2022-10-22 23:45:07 +0530

    Added Provider and Firebase Auth to Sign In User

--- lib/main.dart
+++ lib/main.dart
@@ -1,87 +1,37 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:leo_anime/API/data_manager.dart';
-import 'package:leo_anime/components/app_bar.dart';
-import 'package:leo_anime/pages/home.dart';
-import 'package:leo_anime/pages/search_page.dart';
+import 'package:leo_anime/pages/authentication/user_model.dart';
+import 'package:leo_anime/services/auth.dart';
+import 'package:leo_anime/services/auth_wrapper.dart';
+import 'package:provider/provider.dart';
 
-void main() {
+// Initialize FirebaseApp 
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+
+  // StreamProvider to listen to User App Changes.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Leo Anime',
-      theme: ThemeData(
-        primaryColor: Colors.white,
+    return StreamProvider<AppUser?>.value(
+      catchError: ((context, error) => null),
+      value: AuthService().user,
+      initialData: null,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Leo Anime',
+        theme: ThemeData(
+          primaryColor: Colors.white,
+        ),
+        home: const AuthWrapper(),
       ),
-      home: const MyHomePage(title: 'Leo Anime'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  DataManager dataManager = DataManager();
-  int selectedIndex = 0;
-  @override
-  Widget build(BuildContext context) {
-    Widget? currentWidget;
-
-    switch (selectedIndex) {
-      case 0:
-        currentWidget = Home(dataManager: dataManager);
-        break;
-      case 1:
-        currentWidget = SearchPage(dataManager: dataManager);
-        break;
-      case 2:
-        currentWidget = const Text("Bookmarks");
-        break;
-    }
-
-    return Scaffold(
-        extendBodyBehindAppBar: true,
-        backgroundColor: const Color.fromRGBO(17, 20, 34, 1),
-        appBar: PreferredSize(
-            preferredSize: const Size.fromHeight(50),
-            child: CustomAppBar(title: widget.title)),
-        bottomNavigationBar: BottomNavigationBar(
-          backgroundColor: const Color.fromRGBO(27, 30, 47, 1),
-          unselectedItemColor: const Color.fromRGBO(66, 64, 253, 1),
-          selectedItemColor: Colors.white,
-          currentIndex: selectedIndex,
-          onTap: (newIndex) {
-            setState(() {
-              selectedIndex = newIndex;
-            });
-          },
-          items: const [
-            BottomNavigationBarItem(
-              label: "Home",
-              icon: Icon(Icons.home),
-            ),
-            BottomNavigationBarItem(
-              label: "Search",
-              icon: Icon(Icons.search),
-            ),
-            BottomNavigationBarItem(
-              label: "Bookmark",
-              icon: Icon(Icons.bookmark_border),
-            ),
-          ],
-        ),
-        body: currentWidget);
-  }
-}
